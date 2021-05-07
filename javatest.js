21f93218a8d4a595e35a762c75cd60f35019f10d
CWilliamsJusticeReskill <ChrisW@SccColorado.org>
2021-05-07 12:58:57 -0600

    pushing up Javascript Homework

---
+++ javatest.js
@@ -0,0 +1,80 @@
+/* console.log("Hello World");
+console.log(1);
+​
+var x = 1;
+console.log(x);
+var y = "cat";
+console.log(y);
+var answer = false;
+console.log(answer);
+var empty = null;
+console.log(empty);
+​
+//type note or comments 
+/**
+ * mult
+ * line 
+ * comments
+ 
+​
+var a = 5;
+var b = 6;
+​
+var sum = a + b;
+console.log(sum);
+​
+var sub = b - a;
+console.log(sub);
+​
+var mul = a * b;
+console.log(mul);
+​
+var complicated = (a + b) * 2;
+console.log(complicated);
+​
+var loction = "I am going" + " to the store";
+console.log(loction);
+console.log(loction.length);
+​
+var string1 = "Hello";
+var string2 = " ";
+var string3 = "World!";
+var combineStrings = string1 + string2 + string3;
+console.log(combineStrings);
+console.log(combineStrings.length);*/
+var a = 1;
+console.log(a);
+
+var b = 3.1473;
+console.log(b);
+
+var c = "This is a string";
+console.log(c);
+
+var d = 1;
+var e = 2.16;
+var f = d + e;
+console.log(f);
+
+var g = d / e;
+console.log(g);
+
+var h = d * e;
+console.log(h);
+
+var i = d - e;
+console.log(d);
+
+var j = d % e;
+console.log(j);
+
+var k = "I Love";
+var l = " ";  
+var m = "Coding";
+var n = k + l + m;
+console.log(n);
+
+var o = "Hello JR3!!";
+var p = 100;
+var q = o + p;
+console.log(q);
